9e96dab5fe6379e4f640cb54e6f67e6c3c59ac33
nhat2105 <nhattruong210503@gmail.com>
2024-03-12 22:21:06 -0600

    Added photo gallery connector

--- fitness-tracker/screens/progressTracker/AddPhotoScreen.jsx
+++ fitness-tracker/screens/progressTracker/AddPhotoScreen.jsx
@@ -1,14 +1,49 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image} from 'react-native'
+import React, { useState } from 'react'
 import Header from '../../components/Header'
 import BackButton from '../../components/BackButton'
 import { theme } from '../../theme'
 import { ChevronDown, Upload } from 'react-native-feather'
 import { useNavigation } from '@react-navigation/native'
+import * as ImagePicker from "expo-image-picker" 
+import { Picker }  from '@react-native-picker/picker'
 
 export default function AddPhotoScreen() {
   const themeColors = theme('purple')
   const navigation = useNavigation();
+  const [image, setImage] = useState();
+
+  const [selectedMonth, setSelectedMonth] = useState(null);
+
+  const months = [
+    'January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'
+  ];
+
+  const onMonthChange = (month) => {
+    setSelectedMonth(month);
+  };
+
+
+  const uploadImage = async () => {
+    try {
+      await ImagePicker.requestCameraPermissionsAsync();
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1,1],
+        quality: 1
+      });
+
+      if (!result.canceled){
+        //save image
+        setImage(result.assets[0].uri);
+      }
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <View>
@@ -27,20 +62,28 @@ export default function AddPhotoScreen() {
           marginBottom: 5, marginLeft: 5, marginRight: 5, alignSelf: "center"}}>View Instruction</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={{backgroundColor: 'white', borderRadius: 20, marginLeft: 20, marginBottom: 20,
+      <TouchableOpacity onPress={() => uploadImage()}
+      style={{backgroundColor: 'white', borderRadius: 20, marginLeft: 20, marginBottom: 20,
          marginRight: 20, marginTop: 10}}>
         <View style={{display: 'flex', flexDirection: 'row'}}>
-          <Text style={{fontSize: 20, fontWeight: 800, color: 'black', marginTop: 5, marginLeft: 10, marginBottom: 10}}>Upload Photo</Text>
-          <Upload stroke={themeColors.bgColor(1)} strokeWidth={3} style={{position: 'absolute', right: 30, top: 5}}/>
+          <Text style={{fontSize: 20, fontWeight: 500, color: 'black', marginTop: 5, marginLeft: 10, marginBottom: 10}}>Upload Photo</Text>
+          <Upload stroke={themeColors.bgColor(1)} strokeWidth={3} style={{position: 'absolute', right: 15, top: 5}}/>
         </View>
       </TouchableOpacity>
-      <TouchableOpacity style={{backgroundColor:'white', borderRadius: 20, marginLeft: 20, marginRight: 20, marginTop: 10}}>
-        <View style={{display: 'flex', flexDirection: 'row'}}>
-          <Text style={{fontSize: 20, fontWeight: 800, color: 'black', marginTop: 5, marginLeft: 10, marginBottom: 10}}>Month</Text>
-          <ChevronDown stroke={themeColors.bgColor(1)} strokeWidth={3} style={{position: 'absolute', right: 30, top: 5}}/>
-        </View>
-      </TouchableOpacity>
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200, alignSelf: 'center', marginBottom: 10}} />}
+      <View style={{backgroundColor:'white', borderRadius: 20, marginLeft: 20, marginRight: 20, marginTop: 10}}>
+        <Picker onValueChange={onMonthChange} selectedValue={selectedMonth}>
+        {selectedMonth === null && <Picker.Item label="Month" value={null} />}
+            {months.map((month, index) => (
+              <Picker.Item key={index} 
+              label={month} 
+              style={{ fontSize: 18, fontWeight: '800', color: 'black', marginLeft: 10, marginBottom: 10 }}
+              value={month} />
+            ))}
+        </Picker>
+      </View>
       <BackButton text={"Done"} />
+      {/* Try to add month and photo into photo gallery */}
     </View>
   )
 }
